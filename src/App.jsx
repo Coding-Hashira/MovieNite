0bfd93c8bf6f8401cfc5c3fa87690b2a5e39d736
Coding-Hashira <96920778+Coding-Hashira@users.noreply.github.com>
2022-10-14 14:33:30 +0530

    Chakra UI and React Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Box bgColor={"blackAlpha.900"} h="100vh" color="white">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles/index.css";
+import theme from "./styles/theme";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme?.config?.initialColorMode} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
-)
+);
